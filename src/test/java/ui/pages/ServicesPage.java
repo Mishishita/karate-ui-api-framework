35f8d9a93cec2bb48332fc491773f55ab0412dd9
Mariluz Tinoco <marytnc0507@gmail.com>
2026-06-19 15:51:34 -0500

    feat: add services page navigation test with Playwright

---
+++ src/test/java/ui/pages/ServicesPage.java
@@ -0,0 +1,37 @@
+package ui.pages;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
+public class ServicesPage {
+
+    private final Page page;
+
+    public ServicesPage(Page page){
+        this.page = page;
+
+    }
+    
+
+    public void openHomePage(){
+        page.navigate("https://parabank.parasoft.com/parabank/index.htm");
+    }
+
+    public void clickServices(){
+        
+       page.locator("#headerPanel a")
+        .filter(new Locator.FilterOptions()
+        .setHasText("Services"))
+        .click();
+    }
+
+    public boolean isServicesPageDisplayed(){
+
+        return page.locator("text=Available Bookstore SOAP services:").isVisible();
+    }
+
+    public String getCurrentUrl(){
+        return page.url();
+    }
+
+}
---
+++ src/test/java/ui/tests/ServicesPagePWTest.java
@@ -0,0 +1,44 @@
+package ui.tests;
+
+import org.junit.jupiter.api.Test;
+
+import com.microsoft.playwright.Page;
+
+import ui.base.BaseTest;
+import ui.pages.ServicesPage;
+import utils.LogUtil;
+import java.nio.file.Paths;
+
+
+public class ServicesPagePWTest  extends BaseTest{
+
+    @Test
+    public void shouldOpenServicesPage(){
+
+        ServicesPage servicesPage  = new ServicesPage(page);
+        servicesPage.openHomePage();
+        page.screenshot(new Page.ScreenshotOptions()
+        .setPath(Paths.get("target/homepage.png")));
+
+        LogUtil.info("Página principal abierta");
+
+        servicesPage .clickServices();
+        LogUtil.info("Click en services");
+       
+        if (!servicesPage.getCurrentUrl().contains("services.htm")){
+            throw new RuntimeException("NO se abrio la página services");
+        }
+
+        LogUtil.info("URL correcta:" + servicesPage.getCurrentUrl());
+
+        if (!servicesPage.isServicesPageDisplayed()){
+            throw new RuntimeException("NO se encontró el título de servicios");
+        }
+
+        page.waitForTimeout(5000);
+
+
+    }
+    
+    
+}
