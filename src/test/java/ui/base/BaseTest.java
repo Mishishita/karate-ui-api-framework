708ef5cee18badfdf7e44d564620da9b0fb61c8a
Mariluz Tinoco <marytnc0507@gmail.com>
2026-06-19 12:57:51 -0500

    fix: run playwright tests in headless mode for CI

--- src/test/java/ui/base/BaseTest.java
+++ src/test/java/ui/base/BaseTest.java
@@ -22,8 +22,12 @@ public class BaseTest {
 
         playwright = Playwright.create();
 
+        //browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        boolean isCI = System.getenv("CI") != null;
+
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false));
+                new BrowserType.LaunchOptions()
+                        .setHeadless(isCI));
 
         page = browser.newPage();
 
